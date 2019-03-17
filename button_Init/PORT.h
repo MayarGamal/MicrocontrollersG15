7c1893b2215afb34d44ec454c55a35100a384143
sara <36546098+SaraSafwat@users.noreply.github.com>
2019-03-17 21:39:31 +0200

    Button_Initialization

---
+++ button_Init/PORT.h
@@ -0,0 +1,11 @@
+#ifndef PORT_H_
+#define PORT_H_
+
+#define PORTA 	0x40004000
+#define PORTB 	0x40005000
+#define PORTC 	0x40006000
+#define PORTD 	0x40007000
+#define PORTE 	0x40024000
+#define PORTF 	0x40025000
+
+#endif /*PORT_H_*/ 
